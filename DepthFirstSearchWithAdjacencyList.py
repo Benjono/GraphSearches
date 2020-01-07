22f9cd8c2aa83f0731866c78b72d3994e7497c69
Benjono <56357931+Benjono@users.noreply.github.com>
2020-01-07 20:36:39 +0000

    Update DepthFirstSearchWithAdjacencyList.py

--- DepthFirstSearchWithAdjacencyList.py
+++ DepthFirstSearchWithAdjacencyList.py
@@ -1,19 +1,16 @@
 discovered=[]
 def depthFirstWithAdjacencyList(adjacencyList,start):
     global discovered
-    stack = [start]
     discovered += [start]
     edges=[]
-    while(len(stack)!=0):
-        current=stack[0]
-        stack.pop(0)
-        for i in adjacencyList[current]:
-            #go through each edge
-            if(i not in discovered):
-                #if the vertex hasn't been discovered
-                discovered+=[i] #discover it
-                edges+=[(current,i)] #add the edge
-                edges+=depthFirstWithAdjacencyList(adjacencyList,i) #explore the node
+    current=start
+    for i in adjacencyList[current]:
+        #go through each edge
+        if(i not in discovered):
+            #if the vertex hasn't been discovered
+            discovered+=[i] #discover it
+            edges+=[(current,i)] #add the edge
+            edges+=depthFirstWithAdjacencyList(adjacencyList,i) #explore the node
     return edges
 
 print(depthFirstWithAdjacencyList([[1,2],[0,2],[0,1,3],[2]],0))
